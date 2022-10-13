7692817a70f45fb7fc2ee1419514dbb0a91c87ae
Robert Vásquez <vasquez.robert.andres@gmail.com>
2022-10-13 17:05:58 -0400

    Operator of reference

---
+++ src/reference_operator/NombreUtils.java
@@ -0,0 +1,31 @@
+package reference_operator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Created with IntelliJ IDEA
+ * Created By Robert Vásquez
+ * Date: 13/10/22
+ * Time: 4:46 p. m.
+ */
+
+public class NombreUtils {
+
+  public static void main(String[] args) {
+    List<String> teachers = getList("Thomas", "Jhon", "David");
+
+    Consumer<String> printer = text -> System.out.println(text);
+    teachers.forEach(printer);
+    System.out.println("################################");
+
+    teachers.forEach(System.out::println); // Similar to Consumer printer
+
+  }
+
+  static <T> List<T> getList(T ... elements){
+    return Arrays.asList(elements);
+  }
+
+}
---
+++ src/reference_operator/operator.java
@@ -0,0 +1,12 @@
+package reference_operator;
+
+/**
+ * Created with IntelliJ IDEA
+ * Created By Robert Vásquez
+ * Date: 13/10/22
+ * Time: 4:45 p. m.
+ */
+
+public class operator {
+
+}
