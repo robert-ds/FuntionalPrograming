3c978b7ab8c6405b599b495d0b7f280be07ba7bf
Robert Vásquez <vasquezrober09@gmail.com>
2022-11-09 14:30:32 -0400

    Stream

---
+++ src/stream/Streams.java
@@ -0,0 +1,42 @@
+package stream;
+
+import reference_operator.NameUtils;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Motto: Imagine - Model - Program
+ * Created with IntelliJ IDEA
+ * Created By Robert Vásquez
+ * Date: 9/11/22
+ * Time: 2:05 p. m.
+ */
+
+public class Streams {
+  public static void main(String[] args) {
+
+    List<String> courseList = NameUtils.getList(
+        "java",
+        "Frontend",
+        "Backend",
+        "FullStack"
+    );
+
+    for(String course: courseList){
+      System.out.println("Course: " + course);
+    }
+
+    Stream<String> coursesStream = Stream.of("Java","Frontend","Backend","FullStack");
+
+    // Stream<Integer> courseLengthStream = coursesStream.map(course -> course.length());
+    // Optional<Integer> longest = courseLengthStream.max((x, y) -> y - x);
+
+    Stream<String> emphasisCourse = coursesStream.map(course -> course + "!");
+    Stream<String> justJavaCourses = coursesStream.filter(course -> course.contains("Java"));
+
+    emphasisCourse.forEach(System.out::println);
+
+  }
+
+}
